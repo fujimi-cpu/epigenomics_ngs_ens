2b70bb0ce3dd555d7c391d98e692db9297132c88
fujimi-cpu <jeremie.marlhens@ens-lyon.fr>
2021-11-17 16:13:42 +0000

    instruction fastc

---
+++ src/instruction.sh
@@ -0,0 +1,18 @@
+
+cd ~/mydatalocal/epigenomics_ngs_ens/data/non_published
+
+
+fastqc 2019_007_S7_R1.fastq.gz
+fastqc 2019_007_S7_R2.fastq.gz
+fastqc 2020_372_S2_R1.fastq.gz
+fastqc 2020_372_S2_R2.fastq.gz
+fastqc 2020_380_S10_R1.fastq.gz
+fastqc 2020_380_S10_R2.fastq.gz
+
+
+cd ~/mydatalocal/epigenomics_ngs_ens/data/lu_et_al
+
+SRR4000472_1.fastq
+SRR4000472_2.fastq
+SRR4000473_1.fastq
+SRR4000473_2.fastq
