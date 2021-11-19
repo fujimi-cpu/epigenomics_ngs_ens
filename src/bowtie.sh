77efe4eb1dd66be1414d85d88d75d2c395efceaf
fujimi-cpu <jeremie.marlhens@ens-lyon.fr>
2021-11-19 08:28:05 +0000

    Updating bowtie and trimmomatic script

--- src/bowtie.sh
+++ src/bowtie.sh
@@ -5,3 +5,19 @@ mkdir -p $index_dir
 
 cd $index_dir
 bowtie2-build -f ${workdir}/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa TAIR10
+
+
+workdir=~/mydatalocal/epigenomics_ngs_ens/processed_data/mapped
+
+for f in ~/mydatalocal/epigenomics_ngs_ens/processed_data/trimmed_data/*1_paired.fastq.gz # for each sample
+do
+  input=${f%%1_paired.fastq.gz} # enlever suffixe
+  output=${input/"trimmed_data/"/"mapped/"}
+  echo $input 
+  echo $output
+  echo ${index_dir}
+  bowtie2 -x ${index_dir}/TAIR10 -1 ${input}1_paired.fastq.gz -2 ${input}2_paired.fastq.gz -X 2000 --very-sensitive --threads 6 | samtools sort --output-fmt bam -o ${output}mapped_sorted.bam --threads 6
+done
+
+
+
