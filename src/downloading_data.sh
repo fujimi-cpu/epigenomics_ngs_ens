19b3585be7d1375bf50bc9e305a3e76bfd94531f
fujimi-cpu <55785388+fujimi-cpu@users.noreply.github.com>
2022-01-05 10:22:40 +0100

    Update downloading_data.sh

--- src/downloading_data.sh
+++ src/downloading_data.sh
@@ -1,12 +1,24 @@
-wget
+#downloading data
+cd ~/mydatalocal/epigenomics_ngs_ens/data/non_published
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Data/2020_372_S2_R1.fastq.gz
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Data/2020_372_S2_R2.fastq.gz
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Data/2019_007_S7_R1.fastq.gz
+wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Data/2019_007_S7_R2.fastq.gz
+
+#unzip files
+gunzip -d 2020_372_S2_R1.fastq.gz
+gunzip -d 2020_372_S2_R2.fastq.gz
+gunzip -d 2019_007_S7_R1.fastq.gz
+gunzip -d 2019_007_S7_R2.fastq.gz
 
 
-#download controle qualité
+#download quality control
 cd ~/mydatalocal/epigenomics_ngs_ens/src
 wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Scripts/atac_qc.sh
 wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Scripts/plot_tlen.R
 wget --user='tp_ngs' --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/arabidocontratac/Scripts/plot_tss_enrich.R	
 
+#genome data
 cd ~/mydatalocal/epigenomics_ngs_ens/data/supporting_data
 wget http://ftp.ebi.ac.uk/ensemblgenomes/pub/release-52/plants/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.52.gtf.gz
 gunzip Arabidopsis_thaliana.TAIR10.52.gtf.gz
