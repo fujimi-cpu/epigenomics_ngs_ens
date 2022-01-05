b814bf46621ed5113db553e1348175fe30299bb2
fujimi-cpu <55785388+fujimi-cpu@users.noreply.github.com>
2022-01-05 10:27:44 +0100

    Update filtering.sh

--- src/filtering.sh
+++ src/filtering.sh
@@ -1,9 +1,8 @@
-#Enlever les séquences inintéressantes
-#
+#Remove uninteresting sequences
 
 workdir=~/mydatalocal/epigenomics_ngs_ens/processed_data/mapped
 
-#Marquer les dupliqués  de pcr
+#Mark pcr duplicates
 
 for f in $workdir/*.bam
 do 
@@ -12,25 +11,27 @@ java -jar $PICARD MarkDuplicates \
       O=${f/".bam"/"marked_duplicates.bam"}\
       M=${f/".bam"/"marked_dup_metrics.txt"}
 done
+#picard duplicate marks and removes duplicate regions
 
-#Voila les régions qu'on veut garder, tout sauf le génome mitochondrial et chloroplastique
+#Here are the regions that we want to keep, everything except the mitochondrial and chloroplastic genome
 cd ~/mydatalocal/epigenomics_ngs_ens/data/supporting_data
 grep -v -E "Mt|Pt" TAIR10_selectedRegions.bed > TAIR10_selectedRegions_nuc.bed
-#grep permet de rechercher les lignes dans un fichier, Pt ou Mt, 
+#grep allows you to search for lines in a file, Pt or Mt
+#grep is a selection tool, it is used to remove the mitochondrial and chloroplast genome (select the opposite of the search with -v) and save it with >.
 
-#enlevez les dupliqués 
+#remove duplicates 
 dir_selected=~/mydatalocal/epigenomics_ngs_ens/data/supporting_data
 workdir=~/mydatalocal/epigenomics_ngs_ens/processed_data/mapped
 output_dir=~/mydatalocal/epigenomics_ngs_ens/processed_data/filtered
 
 for f in $workdir/*duplicates.bam
 do
-  file_name="$(basename -- $f)" #basename permet de récupérer le nom du fichier seulement
+  file_name="$(basename -- $f)" #basename allows you to retrieve the name of the file only
   #echo $file_name
   samtools view -b $f -L ${dir_selected}/TAIR10_selectedRegions_nuc.bed -F 1024 -f 3 -q 30 -o ${output_dir}/${file_name/".bam"/"_filtered.bam"} #on enregistre dans output_dir
 done
 
-#samtools pour ouvrir les fichier bam
-#-F on enlève les dupliqués (de pcr)
-#-f on enlève les unpaired (on garde les paired)
-#-q limite de qualité
+#samtools to open bam files
+#-F we remove the duplicates (of pcr)
+#-f we remove the unpaired (we keep the paired)
+#-q quality limit
