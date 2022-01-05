5fdc50eb35fcc47cd5f8b462c9f9b7e382ddbccb
fujimi-cpu <55785388+fujimi-cpu@users.noreply.github.com>
2022-01-05 10:25:00 +0100

    Update analysis_comparison_WOX5-WR.R

--- src/analysis_comparison_WOX5-WR.R
+++ src/analysis_comparison_WOX5-WR.R
@@ -5,15 +5,16 @@ WR=read.table(file = "~/mydatalocal/epigenomics_ngs_ens/processed_data/analysis/
 WOX5=read.table(file = "~/mydatalocal/epigenomics_ngs_ens/processed_data/analysis/2019_007_S7_R.nearest.genes.txt")
 WR=read.table(file = "~/mydatalocal/epigenomics_ngs_ens/processed_data/analysis/2020_374_S4.nearest.genes.txt")
 
-
+#load peakcalling analysis
 
 WOX5$norm=WOX5$V5/sum(WOX5$V5)
 WR$norm=WR$V5/sum(WR$V5)
-
+#normalize data for both conditions
 
 head(WR)
 head(WOX5)
 
+#show peak coverage for both condition in log scale
 plot(log(WOX5$norm), log(WR$norm), col=alpha("black", 0.3), cex=0.1, ylim=c(-14,-8))
 text(log(WOX5$norm), log(WR$norm), labels=WR$V12, cex= 0.3)
 
@@ -27,7 +28,6 @@ library(ggrepel)
 
 WOX5$normwr = WR$norm
 
-WOX5 %>% 
 
   
   ggplot(data=WOX5, mapping=aes(x=log(norm), y=log(normwr), label=V9))+
@@ -53,83 +53,5 @@ ggplot(data=WOX5, mapping=aes(x=log(norm), y=log(normwr), label=V9))+
 tab = WOX5[log(WOX5$norm) - log(WR$norm)>2,]
 write.table(tab, "~/mydatalocal/epigenomics_ngs_ens/processed_data/analysis/WOX5_genes.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("org.At.tair.db")
-library(org.At.tair.db)
 
 
-
-
-## select() interface:
-## Objects in this package can be accessed using the select() interface
-## from the AnnotationDbi package. See ?select for details.
-## Bimap interface:
-x <- org.At.tairGO
-# Get the TAIR gene identifiers that are mapped to a GO ID
-mapped_genes <- mappedkeys(x)
-# Convert to a list
-xx <- as.list(x[mapped_genes])
-if(length(xx) > 0) {
-  # Try the first one
-  got <- xx[[1]]
-  got[[1]][["GOID"]]
-  got[[1]][["Ontology"]]
-  got[[1]][["Evidence"]]
-}
-# For the reverse map:
-# Convert to a list
-xx <- as.list(org.At.tairGO2TAIR)
-if(length(xx) > 0){
-  # Gets the TAIR gene ids for the top 2nd and 3nd GO identifiers
-  goids <- xx[2:3]
-  # Gets the TAIR ids for the first element of goids
-  goids[[1]]
-  # Evidence code for the mappings
-  names(goids[[1]])
-}
-
-test = toTable(xx)
-
-## select() interface:
-## Objects in this package can be accessed using the select() interface
-## from the AnnotationDbi package. See ?select for details.
-## Bimap interface:
-# Convert to a list
-xx <- as.list(org.At.tairGO2ALLTAIRS)
-if(length(xx) > 0){
-  # Gets the tair identifiers for the top 2nd and 3nd GO identifiers
-  goids <- xx[2:3]
-  # Gets all the tair identifiers for the first element of goids
-  goids[[1]]
-  # Evidence code for the mappings
-  names(goids[[1]])
-}
-
-
-head(xx)
-toTable(xx)
-
-xx['GO:0000002']
-xx['AT1G31760']
-
-## select() interface:
-## Objects in this package can be accessed using the select() interface
-## from the AnnotationDbi package. See ?select for details.
-## Bimap interface:
-x <- org.At.tairGENENAME
-# Get the TAIR identifiers that are mapped to a gene name
-mapped_tairs <- mappedkeys(x)
-# Convert to a list
-
-xx <- as.list(x[mapped_tairs])
-if(length(xx) > 0) {
-  # Get the GENENAME for the first five tairs
-  xx[1:5]
-  # Get the first one
-  xx[[1]]
-}
-head(xx)
-xx['AT3G11260']
-
-BiocManager::install("topGO")
